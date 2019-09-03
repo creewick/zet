39c123cc005ed22c15af75148b38070b88922cba
Юхатский Илья Владиславович <ilia.y@skbkontur.ru>
2019-09-03 12:12:44 +0500

    list of modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import getStudyPlan from './getStudyPlan';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+    const [plan, setPlan] = useState({});
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/creewick/zet/master/src/plan.html')
+            .then((r) => r.text())
+            .then(getStudyPlan)
+            .then(setPlan);
+    }, []);
+
+    return (
+      <div className="App">
+          {plan.modules && plan.modules.map(m => <h1>{m.name}</h1>)}
+      </div>
+    );
 }
-
-export default App;
---
+++ src/getStudyPlan.js
@@ -0,0 +1,77 @@
+const isModulesHeader = (tr) => tr.getElementsByClassName('c1')[0].innerText.includes('модули');
+
+const getModule = (tr) => {
+    const cells = tr.getElementsByTagName('td');
+    const name = cells[0].innerText.match(/(М\.\d+\.\d+) (.*)/);
+    const points = parseInt(cells[1].innerText, 10);
+
+    return {
+        code: name[1],
+        name: name[2],
+        points,
+        courses: {
+            required: [],
+            additional: [],
+        },
+    };
+};
+
+const getType = (tr) => {
+    const name = tr.getElementsByTagName('i')[0].innerText;
+
+    if (name.includes('Контроль')) return null;
+    return name.includes('По выбору')
+        ? 'additional'
+        : 'required';
+};
+
+const getCourse = (tr) => {
+    const cells = tr.getElementsByTagName('td');
+    if (cells.length < 4) return null;
+
+    const name = cells[0].innerText.match(/(\d+\.\d+\.\d+) (.*)/);
+    const points = parseInt(cells[1].innerText, 10);
+    const types = cells[2].innerText.split(', ');
+    const semesters = cells[3].innerText.split(', ').map(parseInt);
+
+    return {
+        code: name[1],
+        name: name[2],
+        points,
+        types,
+        semesters,
+    };
+};
+
+export default (htmlAsString) => {
+    const html = document.createElement('html');
+    html.innerHTML = htmlAsString;
+
+    const table = html.getElementsByClassName('edication-plan')[0];
+    const lines = table.getElementsByTagName('tr');
+
+    const result = { modules: [] };
+    let curModule = null;
+    let curType = null;
+
+    for (const tr of lines) {
+        const type = tr.className;
+
+        if (type === 'tr-header' && !isModulesHeader(tr)) break;
+        if (type === 'tr-second-header') {
+            curModule = getModule(tr);
+            result.modules.push(curModule);
+        }
+        if (type === 'tr-third-header') {
+            curType = getType(tr);
+        }
+        if (type === '') {
+            const course = getCourse(tr);
+            if (curModule && curType && course) {
+                curModule.courses[curType].push(course);
+            }
+        }
+    }
+
+    return result;
+};
