d4e65e9358d1d37bf88cbb142b9dc2d4a79ec4e6
creewick <ilia_Y@mail.ru>
2019-09-05 17:15:23 +0500

    new build

---
+++ docs/build/precache-manifest.a451d7e57adbbe5b2a49cc9b584f6e70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b3e616f56dbfac2b30ce3ad4bd24c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9944146dbcbcdfac859",
+    "url": "/static/js/2.03625032.chunk.js"
+  },
+  {
+    "revision": "b9eeb0c2a7e6cc73aac5",
+    "url": "/static/js/main.ffdd80c2.chunk.js"
+  },
+  {
+    "revision": "c864060fb54c53fd9b33",
+    "url": "/static/js/runtime~main.f0c6804c.js"
+  }
+]);
